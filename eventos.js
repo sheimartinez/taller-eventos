f7053b8535b033c1094d420acdca703c1e3b64f0
SheilaMart <sheilamartinez372@gmail.com>
2025-08-16 00:47:23 -0300

    Solución del click

--- eventos.js
+++ eventos.js
@@ -2,4 +2,7 @@ const contenedor = document.getElementById('contenedor');
 contenedor.addEventListener('click', () => {
     alert('Hola! Soy el div');
 });
-
+const boton = document.getElementById('btn-saludar');
+boton.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
